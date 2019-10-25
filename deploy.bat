01c50c8b382da9b9bd83099010b30f7bce075419
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-10-25 06:30:14 -0400

    Updated MiniAVC to 1.4.0

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
-@echo off
+rem@echo off
 
 rem H is the destination game folder
 rem GAMEDIR is the name of the mod folder (usually the mod name)
@@ -6,7 +6,7 @@ rem GAMEDATA is the name of the local GameData
 rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
 rem    but not always
 
-set H=%KSPDIR%v
+set H=%KSPDIR%
 set GAMEDIR=CorrectCOL
 set GAMEDATA="GameData"
 set VERSIONFILE=%GAMEDIR%.version
